7203f8756239c303f5052e01732cb07e8a46c7ad
Puneet <142662283+Puneet-chauhan01@users.noreply.github.com>
2025-02-16 14:29:08 +0530

    fixed update profile
    using multer for large image uploads

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -12,15 +12,32 @@ const ProfilePage = () => {
 
     const reader = new FileReader();
 
-    reader.readAsDataURL(file);
 
     reader.onload = async () => {
-      const base64Image = reader.result;
-      setSelectedImg(base64Image);
-      await updateProfile({ profilePic: base64Image });
+      setSelectedImg(reader.result);
     };
+    reader.readAsDataURL(file);
+    const formData = new FormData();
+    formData.append('profilePic', file);
+
+    await updateProfile(formData);
+
 
   };
+
+
+  // const handleImageUpload = async (e) => {
+  //   const file = e.target.files[0];
+  //   if (!file) return;
+    
+  //   const formData = new FormData();
+  //   formData.append('profilePic', file);
+    
+  //   // Assuming updateProfile accepts FormData:
+  //   await updateProfile(formData);
+  // };
+
+  
   return (
     <div className='h-screen pt-20'>
       <div className='max-w-2xl mx-auto p-4 py-8'>
